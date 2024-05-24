0a2a6b73aca94c2fe4d0a55cd8c13f6694d302f5
Diego Lechuga <diegolechuga2003@gmail.comgit config --list>
2024-05-23 21:42:03 -0600

    feat: API's con ejercicios

--- 4.2.2_ConsumoDeApis/api.js
+++ 4.2.2_ConsumoDeApis/api.js
@@ -1,16 +1,24 @@
 let image = document.getElementById("character-image");
 let characterName = document.querySelector("#character-name");
+let button = document.querySelector("#random");
+
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
 
 const getData = async () => {
     try {
-        let response = await fetch("https://rickandmortyapi.com/api/character");
+        let numeroAleatorio = getRandomInt(0, 800);
+        let response = await fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`);
         let data = await response.json();
         console.log(data);
-        characterName.innerHTML = data.results[1].name;
-        image.src = data.results[1].image;
+        characterName.innerHTML = data.name;
+        image.src = data.image;
     } catch (error) {
         console.log(error);
     }
 }
 
-getData();
+button.addEventListener("click", () => getData())
