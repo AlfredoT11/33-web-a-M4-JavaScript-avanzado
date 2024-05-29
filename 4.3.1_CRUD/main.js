ef69bf9dc24e577abd3e6698a3289b035efb59e6
Diego Lechuga <diegolechuga2003@gmail.comgit config --list>
2024-05-28 21:35:50 -0600

    feat: CRUD Completo

--- 4.3.1_CRUD/main.js
+++ 4.3.1_CRUD/main.js
@@ -1,10 +1,23 @@
 const btnShowUsers = document.querySelector("#show-users-button");
 const formCreate = document.querySelector("#form-create");
+const formUpdate = document.querySelector("#form-update");
+const formDelete = document.querySelector("#form-delete");
+const usersContainer = document.querySelector("#users-container");
 
 const getUsers = async () => {
     const response = await fetch("https://crudcrud.com/api/d00eecceb4c04121888888ff9f1dfd60/users");
     const data = await response.json();
     console.log(data);
+    let users = data;
+    users.map((item) => {
+        usersContainer.insertAdjacentHTML("beforeend", `
+        <div class="border-2 border-blue-500 m-5 p-5 w-64 ">
+            <p>id: ${item._id}</p>
+            <h1>nombre: ${item.username}</h1>
+            <p>Password: ${item.password}</p>
+        </div>
+    `)
+    })
 }
 
 const postUser = async (e) => {
@@ -29,5 +42,32 @@ const postUser = async (e) => {
     };
 }
 
+const updateUser = (e) => {
+    e.preventDefault();
+    const id = document.querySelector("#id-input-update").value;
+    const username = document.querySelector("#username-input-update").value;
+    const password = document.querySelector("#password-input-update").value;
+    fetch(`https://crudcrud.com/api/d00eecceb4c04121888888ff9f1dfd60/users/${id}`, {
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+        },
+        method: "PUT",
+        body: JSON.stringify({
+            username: username,
+            password: password,
+        }),
+    });
+}
+
+const deleteUser = (e) => {
+    e.preventDefault();
+    const id = document.querySelector("#id-input-delete").value;
+    fetch(`https://crudcrud.com/api/d00eecceb4c04121888888ff9f1dfd60/users/${id}`, {
+        method: "DELETE",
+    });
+}
+
 btnShowUsers.addEventListener("click", () => getUsers());
 formCreate.addEventListener("submit", (e) => postUser(e));
+formUpdate.addEventListener("submit", (e) => updateUser(e));
+formDelete.addEventListener("submit", (e) => deleteUser(e));
